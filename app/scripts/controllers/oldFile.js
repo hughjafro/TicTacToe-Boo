a8964619d1facf10192e5ef10e8c68f3d6837846
Christopher Tolbert <cmtolbert1@gmail.com>
2013-10-14 14:21:07 -0700

    trying to revert back to old version...help

--- app/scripts/controllers/oldFile.js
+++ app/scripts/controllers/oldFile.js
@@ -2,101 +2,73 @@
 
 angular.module('TicSackToeApp')
   .controller('MainCtrl', function ($scope, angularFire) {
-// binding these to firebase
-    $scope.games = [];
-    $scope.queue = {};
-    //Tell firebase where file url is and bind "games" to firebase: $scope.games
-    var games = new Firebase("https://ticsacktoe.firebaseio.com/games");
-    angularFire(games, $scope, "games").then(function () {
 
-      //Now bind "queue" to firebase: $scope.queue
-      //queue contains the id of the game , not the player
-      var queue = new Firebase("https://ticsacktoe.firebaseio.com/queue");
-      angularFire(queue, $scope, "queue").then(function () {
+	$scope.ticTacToe=[];
 
-        //Checks to see if queue is empty, ie a player already there, if not they start as player 1
-        if ($scope.queue.gameId == undefined) {
-          console.log("I'm player 1");
-          $scope.player = "p1";
+    var database = new Firebase("https://ticsacktoe.firebaseio.com/ticTacToe");
 
-          //create new game
-          var newGame = {
-            board: [[{val:'', r: 0, c: 0},{val:'', r: 0, c: 1},{val:'', r: 0, c: 2}], [{val:'', r: 1, c: 0},{val:'', r: 1, c: 1},{val:'', r: 1, c: 2}], [{val:'', r: 2, c: 0},{val:'', r: 2, c: 1},{val:'', r: 2, c: 2}]],
-            turn: 'p1',
-            win: false,
-            turnCount: 0,
-            waiting: true
-          };
-
-          // its -1 bcs push will return the length but my index starts at 0;
-          //appends to array of games, gameId will keep total games count
-          $scope.gameId = $scope.games.push(newGame) - 1;
-
-          //says here is the game index that we can hold and sync with next player
-          $scope.queue.gameId = $scope.gameId;
-          console.log("Player 1's game is: " + $scope.gameId);
-
-        } else {
-          console.log("I'm player 2");
-          $scope.player = "p2";
-
-          //if someone is already in the queue then will make p2 gameId the same as p1
-          $scope.gameId = $scope.queue.gameId;
-
-          //now clear the queue to get ready for the nextgame
-          $scope.queue = {};
-          console.log("Player 2's game is: " + $scope.gameId);
-        }
-      });
-
+    angularFire(database, $scope, "ticTacToe").then(function()
+    {
+    	$scope.ticTacToe=[[{val:'', r: 0, c: 0},{val:'', r: 0, c: 1},{val:'', r: 0, c: 2}], [{val:'', r: 1, c: 0},{val:'', r: 1, c: 1},{val:'', r: 1, c: 2}], [{val:'', r: 2, c: 0},{val:'', r: 2, c: 1},{val:'', r: 2, c: 2}]];
     });
        
+    // $scope.room =
+    // {
+    // 	playerTurn: 1,
+    // 	ticTacToe:[[{val:'', r: 0, c: 0},{val:'', r: 0, c: 1},{val:'', r: 0, c: 2}], [{val:'', r: 1, c: 0},{val:'', r: 1, c: 1},{val:'', r: 1, c: 2}], [{val:'', r: 2, c: 0},{val:'', r: 2, c: 1},{val:'', r: 2, c: 2}]],
+    // 	playerNum: 0,
+    // 	gameWin: false,
+    // 	gameTie: false  
+    // }
 
 
-		// $scope.gameWin = function() {
-		// 	$scope.winningCombo: true;
-		// 	$scope.winNotify: true;
-		// };
+		$scope.gameWin = function() {
+			$scope.winningCombo: true;
+			$scope.winNotify: true;
+		};
 
-		// $scope.gameTie = function() {
-		// 	$scope.winningCombo: true;
-		// 	$scope.winNotify: true;
-		// };
+		$scope.gameTie = function() {
+			$scope.winningCombo: true;
+			$scope.winNotify: true;
+		};
 
     // $scope.$watch('room.gameWin') function() {
 
 
     // }
 
-	
+	var turn = 0;
 
 	$scope.clickSquare = function(cell) {
-		var move = 0;
 
 		// $scope.ticTacToe[row][column] = cell;
 		// $scope.ticTacToe = cell;
+		
+	// var letter;
 
 	if (cell.val !="") {
 		console.log(cell.val);
 	 	return;
 	};
 
-	if ((move%2==0) && (move<9))
+	if ((turn%2==0) && (turn<9))
 		cell.val = "X";
 	else 
 		cell.val = "O";
 
 	// $scope.ticTacToe[row][column] = letter;
-	$scope.board[cell]=cell.val;
+	$scope.ticTacToe[cell]=cell.val;
 	
-	$scope.winningCombo(this.board);
+	$scope.winningCombo(this.ticTacToe);
 	
-	move++
-		if(move==9)
+	turn++
+		if(turn==9)
 			alert("Game over no more moves");
 	}; 
 
-// might need to add this scope.move = {number: 1};
+// might need to add this scope.turn = {number: 1};
+
+
 	$scope.winningCombo = function() {
 		//Diagonal
 		if($scope.ticTacToe[1][1].val != ""){
